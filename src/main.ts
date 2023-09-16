95e00ddf8363717899a410db32b133642a1f6cf8
y.borovskova <y.borovskova@mycryptobook.io>
2023-09-16 18:06:55 +0300

    init app

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/platform-ws';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Postgres/prisma education')
+    .setDescription('Postgres/prisma API description')
+    .setVersion('1.0')
+    .addTag('Postgres/prismaEducation')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+  app.useWebSocketAdapter(new WsAdapter(app));
+
+  await app.listen(3000);
+}
+bootstrap();
